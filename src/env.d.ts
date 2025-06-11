e25901b107a969d19c47ff20b670ed3f143f8234
Aleksandar Vasilic <avasilic99@gmail.com>
2025-06-11 23:59:35 +0200

    Add case studies data structure and type definitions for improved project documentation

--- src/env.d.ts
+++ src/env.d.ts
@@ -15,5 +15,25 @@ type ApproachItem = {
     desc2: string | null;
 }
 
+type CaseStudy = {
+    title: string;
+    subtitle: string;
+    image: string;
+    challenge: CaseStudyItem;
+    role: CaseStudyItem;
+    outcome: CaseStudyItem;
+}
+
+type CaseStudyItem = {
+    desc: string
+    bullets: CaseStudyBulletItem[];
+}
+
+type CaseStudyBulletItem = {
+    title: string;
+    desc: string
+}
+
+
 // Astro types, not necessary if you already have a `tsconfig.json`
 /// <reference path="../.astro/types.d.ts" />
