bd55cea848a27d61b2a996de6eff0e41a1db588e
Aleksandar Vasilic <avasilic99@gmail.com>
2025-06-11 20:02:33 +0200

    Add TypeScript support and update build process in package.json

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+// Custom types declarations
+// declare var myString: string;
+
+type NavLink = {
+    href: string;
+    name: string;
+};
+
+// Astro types, not necessary if you already have a `tsconfig.json`
+/// <reference path="../.astro/types.d.ts" />
